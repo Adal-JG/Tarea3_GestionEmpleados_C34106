98b362c1d6b110be708424a6e7f569ae2b6e9215
Elfred Adal Juárez González <elfredjuarezg@gmail.com>
2026-05-02 00:51:12 -0600

    Implementación de modelo y configuracion inicial de datos

--- Tarea3_GestionEmpleados_C34106/Data/AppDbContext.cs
+++ Tarea3_GestionEmpleados_C34106/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Tarea3_GestionEmpleados_C34106.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea3_GestionEmpleados_C34106.Models;
+
+namespace Tarea3_GestionEmpleados_C34106.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
     }
 }
--- Tarea3_GestionEmpleados_C34106/Models/Empleado.cs
+++ Tarea3_GestionEmpleados_C34106/Models/Empleado.cs
@@ -1,6 +1,30 @@
-﻿namespace Tarea3_GestionEmpleados_C34106.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea3_GestionEmpleados_C34106.Models
 {
     public class Empleado
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(80, ErrorMessage = "El nombre no puede superar los 80 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Los apellidos son obligatorios")]
+        [StringLength(100, ErrorMessage = "Los apellidos no pueden superar los 100 caracteres")]
+        public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio")]
+        public string Departamento { get; set; }
+
+        [Required(ErrorMessage = "El salario es obligatorio")]
+        [Range(400000, 10000000, ErrorMessage = "El salario debe estar entre 400000 y 10000000")]
+        public decimal Salario { get; set; }
+
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+
+        public bool Activo { get; set; } = true;
+
+        public string NombreCompleto => $"{Nombre} {Apellidos}";
     }
 }
