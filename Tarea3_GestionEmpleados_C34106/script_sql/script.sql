fda1c0579792e85d5c7890c65f2c16f3959670ea
Elfred Adal Juárez González <elfredjuarezg@gmail.com>
2026-05-02 01:01:46 -0600

    Agregación de script SQL de base de datos y Program.cs

--- Tarea3_GestionEmpleados_C34106/script_sql/script.sql
+++ Tarea3_GestionEmpleados_C34106/script_sql/script.sql
@@ -7,16 +7,16 @@ GO
 USE GestionEmpleadosDB;
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Empleados' AND xtype='U')
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Empleados' AND xtype = 'U')
 BEGIN
     CREATE TABLE Empleados (
         Id INT IDENTITY(1,1) PRIMARY KEY,
         Nombre NVARCHAR(80) NOT NULL,
         Apellidos NVARCHAR(100) NOT NULL,
-        Departamento NVARCHAR(100) NOT NULL,
+        Departamento NVARCHAR(MAX) NOT NULL,
         Salario DECIMAL(18,2) NOT NULL,
-        FechaIngreso DATETIME2 NOT NULL DEFAULT GETDATE(),
-        Activo BIT NOT NULL DEFAULT 1
+        FechaIngreso DATETIME2 NOT NULL,
+        Activo BIT NOT NULL
     );
 END
 GO
